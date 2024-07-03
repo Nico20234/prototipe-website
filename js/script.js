e5a1c7e963b1faf4f8d2cd960ab0f98baf976531
diegogdiaz <diazdiegogabriel@gmail.com>
2024-07-03 20:16:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,45 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 }); 
+
+
+/*
+<script>
+const URL = "http://127.0.0.1/5000"
+
+//const URL = "https://USUARIO.pythonanywhere.com/"
+
+// Capturamos el evento de envío del formulario
+document.getElementById('formulario').addEventListener('submit', 
+function (event) {
+        event.preventDefault(); // Evitamos que se envie el form
+        var formData = new FormData(this);
+// Realizamos la solicitud POST al servidor
+        fetch(URL + 'usuarios', {
+        method: 'POST',
+        body: formData 
+    })
+
+    .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Error al registrar el Usuario.');}
+    })
+
+
+    .then(function (data) {
+        alert('Usuario registrado correctamente.');
+    })
+
+    .catch(function (error) {
+        alert('Error al registrar el Usuario.');
+    })
+
+    .finally(function () {
+        document.getElementById('email').value = "";
+        document.getElementById('password').value = "";
+    });
+})
+</script>
+*/
